e62a8b26fba4b92fe72403eb7202eb486153a61b
willian <will.oliver1994@hotmail.com>
2020-04-16 15:31:36 +0100

    tratativa de erros globais

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserServices from '../services/CreateUserService';
+import ensureAuthenticateed from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -22,5 +27,22 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+// atualizar informação única
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticateed,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+    delete user.password;
+    return response.json(user);
+  },
+);
 
 export default usersRouter;
--- src/services/CreateAppointmentServices.ts
+++ src/services/CreateAppointmentServices.ts
@@ -1,8 +1,8 @@
-import { startOfHour, parseISO } from 'date-fns';
+import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
-import appointmentsRouter from '../routes/appointments.routes';
+import AppError from '../errors/AppError';
 
 interface Request {
   provider_id: string;
@@ -19,7 +19,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error('This Appointment is already booked');
+      throw new AppError('This Appointment is already booked');
     }
 
     const appointment = appointmentRepository.create({
